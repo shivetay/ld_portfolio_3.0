132fccf661ba5efcf3223f0893a1b902bdfc66c0
Łukasz Dawidowicz <lddev@outlook.com>
2021-02-24 14:06:17 +0100

    add modal and transitions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 'use strict';
 
 import './main.scss';
+
+const app = {
+  modalInit: function () {
+    const contactBtn = document.getElementById('contactLi');
+    const contactModal = document.querySelector('.contact');
+    contactBtn.addEventListener(
+      'click',
+      (e) => {
+        contactModal.classList.add('active');
+        contactModal.classList.remove('inactive');
+        contactModal.classList.remove('out');
+        console.log(e.target);
+      },
+      true
+    );
+
+    window.onclick = function (e) {
+      if (e.target == contactModal) {
+        contactModal.classList.add('inactive');
+        contactModal.classList.add('out');
+        contactModal.classList.remove('active');
+      }
+    };
+  },
+  /* app init */
+  init: function () {
+    console.log('*** App starting ***');
+
+    this.modalInit();
+  },
+};
+
+app.init();
